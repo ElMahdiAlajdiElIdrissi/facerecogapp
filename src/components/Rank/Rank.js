cdde4ea78a1658427576ca05cca623cec0ab2a3a
El Mahdi Alajdi El Idrissi <elmahdi.alajdielidrissi@gmail.com>
2020-04-19 20:37:59 +0200

    updating

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Rank = () => {
+const Rank = ({name, entries}) => {
   return (
     <div>
       <div className='white f3'>
-        {'Mahdi, your current rank is...'}
+        {'${this.name}, your current rank is...'}
       </div>
       <div className='white f1'>
-        {'#5'}
+        {entries}
       </div>
     </div>
   );
