de088ca06c61bd4d9c1a4cf2de4ea6859d4ce3f9
El Mahdi Alajdi El Idrissi <elmahdi.alajdielidrissi@gmail.com>
2020-04-22 21:09:03 +0200

    almost final push

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,7 +4,7 @@ const Rank = ({name, entries}) => {
   return (
     <div>
       <div className='white f3'>
-        {'${this.name}, your current rank is...'}
+        {name + ', your current rank is...'}
       </div>
       <div className='white f1'>
         {entries}
